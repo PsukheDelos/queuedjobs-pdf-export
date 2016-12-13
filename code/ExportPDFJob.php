49a9d1a6f05d09e76bbb95539189101676597466
Glen Peek <glen@silverstripe.com>
2016-12-13 17:09:31 +1300

    Update details for PDF Job

--- code/ExportPDFJob.php
+++ code/ExportPDFJob.php
@@ -5,218 +5,94 @@
  */
 class ExportPDFJob extends AbstractQueuedJob implements QueuedJob {
 
-    protected $file;
-    protected $dataList;
-    protected $fields;
+    //Export PDF Variables
+    protected $pages;
 
     /**
      * Constructor
      */
     public function __construct() {
-        $this->dataList = $this->getDataList();
+//        $this->pages =  Page::get();
+        $this->pages = Versioned::get_by_stage('Page', 'Live');
     }
 
     /**
      * @return string
      */
     public function getTitle() {
-        return 'Export objects to CSV';
+        return 'Export all pages to PDF';
     }
 
     /**
-     * Return a signature for this queued job
-     *
-     * @return string
-     */
-    public function getSignature() {
-        return md5(get_class($this) . serialize($this->jobData) . serialize($this->dataList));
-    }
-    
-    /**
-     * Return "Queued" job type
-     */
-    public function getJobType() {
-        return QueuedJob::QUEUED;
-    }
-
-    /**
-     * Gathers data to be exported and returns data as ExportCSVObject
-     * @return DataList
-     */
-    protected function getDataList() {
-        return DataList::create('SiteTree');
-    }
-
-    /**
-     * Returns fields to be exported
-     * @return Array
-     */
-    protected function getFields($object) {
-        return array_merge(array('ID' => 'ID'), $object->summaryFields());
-    }
-
-    protected function getFilePath() {
-        return ASSETS_PATH . '/' . $this->getSignature() . '.csv';
-    }
-
-    /**
-     * Return number of objects to process on each iteration.
-     * this can be overwritten to work with more or less data per iteration.
-     */
-    public function getNumberToProcess() {
-        return 100;
-    }
-
-    /**
-     * Setup this queued job. This is only called the first time this job is executed
-     * (ie when currentStep is 0)
-     */
-    public function setup(){
-        parent::setup();
-
-        // Start from beginning of records
-        $this->currentStep = 0;
-        $this->dataList = $this->getDataList();
-
-        if (file_exists($this->getFilePath())){
-            $this->addMessage("CSV already generated");
-            return false;
-        }
-
-        if(empty($this->dataList)) {
-            $this->addMessage("No Datalist data", 'ERROR');
-            return;
-        }
-
-
-        $this->totalSteps = $this->dataList->count();
-        $this->fields = $this->getFields($this->dataList->first());
-
-
-        if (empty($this->fields)) {
-            $this->addMessage("No fields defined", 'ERROR');
-            return;
-        }
-        
-        if (!$this->file = fopen($this->getFilePath(), 'a')){
-            $this->addMessage("Cannot open file: " . $this->getFilePath(), 'ERROR');
-            return;
-        }
-        
-        // Put header info
-        if (!fputcsv($this->file, $this->fields)) {
-            $this->addMessage("Unable to write data to: " . $this->getFilePath(), 'ERROR');
-            return;
-        }
-    }
-
-    /**
-     * process per-line data to be exported to CSV
-     * return Array
-     */
-    public function getExportData($object) {
-        $data = array();
-
-        foreach ($this->fields as $key => $field) {
-            $data[] = $object->getField($key) ?: '';
-        }
-
-        return $data;
-    }
-
-    /**
-     * Run when an already setup job is being restarted.
-     */
-    public function prepareForRestart() {
-        parent::prepareForRestart();
-
-        $this->dataList = $this->getDataList();
-
-        if(empty($this->dataList)) {
-            $this->addMessage("No Datalist data", 'ERROR');
-            return;
-        }
-
-        $this->fields = $this->getFields($this->dataList->first());
-
-
-        if (empty($this->fields)) {
-            $this->addMessage("No fields defined", 'ERROR');
-            return;
-        }
-        
-        if (!$this->file = fopen($this->getFilePath(), 'a')){
-            $this->addMessage("Cannot open file: " . $this->getFilePath(), 'ERROR');
-            return;
-        }
-        
-        // Put header info
-        if (!fputcsv($this->file, $this->fields)) {
-            $this->addMessage("Unable to write data to: " . $this->getFilePath(), 'ERROR');
-            return;
-        }
-    }
-
-    /**
-     * Process export to CSV
-     *
+     * Process export to PDF
      */
     public function process() {
-        ini_set('max_execution_time', -1);
+        $this->pages->each(function($obj){
 
-        if (!empty($this->messages)) {
-            $this->isComplete = true;
-            return;
-        }
+            SSViewer::set_theme('default');
+            $obj->RenderWith('Page');
+            ModelAsController::controller_for($obj)->generatePDF();
+// $this->
+//            $ch = curl_init();
+//
+//            // set URL and other appropriate options
+//            curl_setopt($ch, CURLOPT_URL, $obj->AbsoluteLink() . 'downloadpdf');
+////            var_dump($obj->AbsoluteLink() . 'downloadpdf'); die;
+//            curl_setopt($ch, CURLOPT_HEADER, 0);
+//            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+////            file_get_contents($obj->AbsoluteLink() . 'downloadpdf');
+//
+//            // grab URL and pass it to the browser
+//            curl_exec($ch);
+//
+//            // close cURL resource, and free up system resources
+//                        curl_close($ch);
 
-        // If dataList / fields has not been populated as part of setup then exit.
-        if(empty($this->dataList) || empty($this->fields) || $this->totalSteps <= 0) {
-            $this->addMessage("No data", 'ERROR');
-            $this->isComplete = true;
-            return;
-        }
+//            Director::getControllerForURL($obj->AbsoluteLink())->generatePDF();
+//            ModelAsController::controller_for($obj)->generatePDF();
+//            die;
+//            var_dump(SSViewer::current_theme()); die;
+//            SSViewer::set_theme('default');
+//            ModelAsController::controller_for($obj)->generatePDF();
+//            var_dump('test'); die;
+//            $obj = $obj->renderWith('nmit');
+//            var_dump($obj); die;
+//            $obj = ModelAsController::controller_for($obj);
+//            $obj->generatePDF();
+//            $obj2 = new BasePage_Controller($obj);
+//            $obj2->generatePDF();
+//            var_dump
+//            var_dump("hi"); die;
+//            $obj->genPDF();
+//            die("hi");
 
-        $processCount = 0;
+//            var_dump($obj->getRawFAQCategories()); die;
+//            var_dump("process"); die;
+//            Debug::dump($obj); die;
+//            var_dump(ModelAsController::controller_for($obj,'generatePDF'));die;
+//            Director::test($obj->);
+//            var_dump($obj->AbsoluteLink() . 'downloadpdf'); die;
+//            Director::
+//            var_dump(Director::test($obj->AbsoluteLink() . 'downloadpdf'));
+//            ModelAsController::controller_for($obj)->dataRecord->getPdfFilename();
 
-        // Export X number of objects in dataList
-        // the database every time
-        while ($this->currentStep < $this->totalSteps) {
+//            var_dump($obj->getController());
+//            $path = ModelAsController::controller_for($obj)->dataRecord->getPdfFilename();
+//            if(!file_exists($path)) {
+//
+//                ModelAsController::controller_for($obj,'generatePDF');
+////                $obj->generatePDF();
+//            }
+//            ModelAsController::controller_for($obj)->generatePDF();
 
-            if (!$object = $this->dataList->offsetGet($this->currentStep)){
-                $this->addMessage("No offset found: ". $this->currentStep, 'ERROR');
-                return false;
-            }
+            //create jobs that create follow on jobs
+            //create another job that does next 100
+            //reason to separate out the feature
 
-            if (!$object->exists()) {
-                $this->addMessage("No Object found", 'ERROR');
-                return false;
-            }
+        });
 
-            // Get Export data
-            if (!$exportData = $this->getExportData($object)) {
-                $this->isComplete = true;
-                return;
-            }
-
-            // Do export of data
-            if (!fputcsv($this->file, $exportData))
-            {
-                $this->addMessage("Unable to write data to: " . $this->getFilePath(), 'ERROR');
-                $this->isComplete = true;
-                return;
-            }
-
-            $this->currentStep++;
-            $processCount++;           
-
-            if ($processCount >= $this->getNumberToProcess()) {
-                break;
-            }
-        }
-
-        if ($this->currentStep >= $this->totalSteps) {
-            $this->isComplete = true;
-            return;
-        }
+        $this->isComplete = true;
+        return;
     }
+
 }
