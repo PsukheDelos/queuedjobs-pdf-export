ae093c3b815ff8d083d77e4f79e2e8a57172152b
Glen Peek <glen@silverstripe.com>
2016-12-20 13:55:15 +1300

    Adding in comments and attempting to manually set SSViewer theme

--- code/ExportPDFJob.php
+++ code/ExportPDFJob.php
@@ -3,7 +3,8 @@
  * This queued job is to export data into a CSV for downloading.
  *
  */
-class ExportPDFJob extends AbstractQueuedJob implements QueuedJob {
+class ExportPDFJob extends AbstractQueuedJob implements QueuedJob
+{
 
     //Export PDF Variables
     protected $pages;
@@ -11,15 +12,18 @@ class ExportPDFJob extends AbstractQueuedJob implements QueuedJob {
     /**
      * Constructor
      */
-    public function __construct() {
+    public function __construct()
+    {
         //$this->pages =  Page::get();
+        //Get all Live Pages
         $this->pages = Versioned::get_by_stage('Page', 'Live');
     }
 
     /**
      * @return string
      */
-    public function getTitle() {
+    public function getTitle()
+    {
         return 'Export all pages to PDF';
     }
 
@@ -28,12 +32,35 @@ class ExportPDFJob extends AbstractQueuedJob implements QueuedJob {
      */
     public function process()
     {
-        $this->pages->each(function ($page) {
-            Director::test($page->AbsoluteLink() . 'downloadpdf');
+        //@TODO: Limit this to processing 100 pages, then create a new job to process next 100 until done.
+        //this is one possible albeit hacky solution, would still need a work around for home page
+//        $this->pages->each(function ($page) {
+//            Director::test($page->AbsoluteLink() . 'downloadpdf');
+//        });
+
+
+        $oldEnabled = Config::inst()->get('SSViewer', 'theme_enabled');
+        $oldTheme = Config::inst()->get('SSViewer', 'theme');
+
+        Config::inst()->update('SSViewer', 'theme_enabled', true);
+        Config::inst()->update('SSViewer', 'theme', 'new'); //experiment to manually set theme while processing pdf
+
+
+        //This is main piece of code
+        //--------------------------
+        $this->pages->limit(1)->each(function ($page) { //limiting to 1 page while testing
+
+            $page_controller = ModelAsController::controller_for($page);
+            $page_controller->generatePDF();
+
         });
+        //--------------------------
+
+        Config::inst()->update('SSViewer', 'theme_enabled', $oldEnabled);
+        Config::inst()->update('SSViewer', 'theme', $oldTheme);
+
 
         $this->isComplete = true;
         return;
     }
-
 }
